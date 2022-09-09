5302c6555ab910acf11ac545ae6b64c8341a3e3d
Thierry Sans <thierry.sans@gmail.com>
2022-09-09 11:16:33 -0400

    lecture 1

---
+++ lectures/01/examples/hash.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# using the PyNaCl: Python binding to the libsodium library
+# https://pynacl.readthedocs.io/en/latest/
+
+from nacl.encoding import Base64Encoder
+from nacl.hash import sha256
+
+msg = b'Hi, I am Alice'
+digest_base64 = sha256(msg, encoder=Base64Encoder)
+print(digest_base64.decode('utf-8'))
+
+msg = b'Hi, I am Alice!'
+digest_base64 = sha256(msg, encoder=Base64Encoder)
+print(digest_base64.decode('utf-8'))
