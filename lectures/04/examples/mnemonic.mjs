a8d1697b44ed5b9c5c24a46b9d4bc088b0b1941f
Thierry Sans <thierry.sans@gmail.com>
2023-09-29 13:13:51 -0400

    bug fix in example 04

--- lectures/04/examples/mnemonic.mjs
+++ lectures/04/examples/mnemonic.mjs
@@ -1,9 +1,10 @@
 import { generateMnemonic, mnemonicToSeed } from '@scure/bip39';
 import { wordlist } from '@scure/bip39/wordlists/english';
 
-console.log(wordlist.length);
+console.log(JSON.stringify(wordlist, null, 2));
 
 import { HDKey } from '@scure/bip32';
+
 
 const mn = generateMnemonic(wordlist);
 console.log(mn);
--- lectures/04/examples/pbkdf2.mjs
+++ lectures/04/examples/pbkdf2.mjs
@@ -5,6 +5,8 @@ import { sha256 } from '@noble/hashes/sha256';
 const password = "CSCD71R0CK5!"
 const salt = "Where would we be without salt?"
 
-const privKey = pbkdf2(sha256, 'password', 'salt', { c: 32, dkLen: 32 });
+const privKey = pbkdf2(sha256, password, salt, { c: 32, dkLen: 32 });
+
+console.log(privKey);
 
 console.log(secp256k1.privateKeyVerify(privKey))
