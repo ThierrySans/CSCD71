8460bdfde692722142840fe0b63957a01db91608
Thierry Sans <thierry.sans@gmail.com>
2025-01-14 10:57:08 -0500

    lecture 2

--- lectures/01/src/keccak-hash-example.ts
+++ lectures/01/src/keccak-hash-example.ts
@@ -3,7 +3,7 @@ import create from "keccak";
 const keccak = create("keccak224"); 
 
 const message = "Hello, Keccak!";
-const hash = keccak.update("alice").digest('hex');
+const hash = keccak.update(message).digest('hex');
 
 console.log('Message:', message);
 console.log('Keccak Hash:', hash);
